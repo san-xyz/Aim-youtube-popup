4d0f67176986d17b038e5ecb63fac582ec75c680
SAN <72621651+san-xyz@users.noreply.github.com>
2021-12-28 22:00:41 +0530

    Update File.js

--- File.js
+++ File.js
@@ -3,9 +3,18 @@ const CE = _ => {
 }  
 let D1 = CE("div"),
     S0 = CE("style");
-D1.setAttribute("class", "MND")
-D1.setAttribute("id", "MND")
+D1.setAttribute("id", "MNB")
 S0.innerHTML = `
+    #MNB{
+        position:fixed;
+        left:0;
+        top:0;
+        height:100vh;
+        width:100vw;
+        background:rgba(0,0,0,.7);
+        opacity:1;
+        -webkit-transition:opacity .5s ease;
+    }
     .MND{
         height:250px;
         width:250px;
@@ -90,13 +99,9 @@ S0.innerHTML = `
         z-index:3;
         color:white;
     }
-    #MND{
-        opacity:1;
-        -webkit-transition:opacity .5s ease;
-    }    
-
 `;
 D1.innerHTML = `
+    <div class="MND">
     <div style="-webkit-transform:rotate(45deg);z-index:1; " class="MND"></div>
     <div id="YTL">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" id="YTS" viewBox="0 0 16 16"> <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/> </svg>
@@ -105,7 +110,7 @@ D1.innerHTML = `
     <p id="PPT">Subscribe My Youtube Channel</p>
     <a id="YTLK" href="https://youtube.com/c/CodingInfinite">https://youtube.com/c/CodingInfinite</a>
     <div id="OK">OK</div>
-    
+    </div>
 `;
 document.head.appendChild(S0);
 document.body.appendChild(D1);
